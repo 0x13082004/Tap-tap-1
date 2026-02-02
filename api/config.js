162f5811a9711db958616d559174113c0ae353b6
0x13082004 <0x13082004@gmail.com>
2026-02-02 23:10:53 +0600

    Delete api directory

--- api/config.js
+++
@@ -1,8 +0,0 @@
-// Vercel Serverless Function: exposes NON-secret runtime config to the client
-// Set in Vercel project settings: PAYMASTER_SERVICE_URL
-module.exports = (req, res) => {
-  res.setHeader('Cache-Control', 'no-store');
-  res.status(200).json({
-    paymasterServiceUrl: process.env.PAYMASTER_SERVICE_URL || ""
-  });
-};
